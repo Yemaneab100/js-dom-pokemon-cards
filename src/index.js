321ff48774cb2531e85a9403fcdd25b2296c2eb9
= <yemunRussom10@gmail.com>
2022-10-23 21:00:03 +0100

    Final project done

--- src/index.js
+++ src/index.js
@@ -1,37 +1,35 @@
+console.log(data)
 
-console.log(data);
+const cardLists = document.querySelector('.cards');
 
-//You can start simple and just render a single 
-//pokemon card from the first element
-console.log(data[0]);
 
-// select the <ul> for placing pokemon cards
-const ul = document.querySelector('ul');
-
- function createAndAppendPokemonCard(pokemonJSObject) {
-  // create the HTML elements as defined in the template
-  const li = document.createElement('li');
-  console.log('create li ');
-
-  const h2 = document.createElement('h2');
-  console.log('header');
-
-  const img = document.createElement('img');
-  console.log('image')
+  data.forEach(card => {
+    const li = document.createElement('li');
+    li.classList.add('card');
+    const h2 = document.createElement('h2');
+    h2.classList.add('card--title');
+    h2.innerText = card.name;
+    const img = document.createElement('img')
+    img.setAttribute('width', 256);
+    img.classList.add('card--img');
+    const sorImage = card.sprites.other['official-artwork'].front_default;
+    img.setAttribute('src', sorImage);
+    
+    li.appendChild(h2);
+    li.appendChild(img);
+    cardLists.appendChild(li);
   
-  // ensure I put the right attribute values: class, src, width
+    const ul = document.createElement('ul');
+    ul.classList.add('card--text');
+    li.appendChild(ul);
+    card.stats.forEach(stat => {
+      const listText = document.createElement('li');
+      listText.innerText = stat.base_stat + stat.stat.name;
+      ul.appendChild(listText);
+      
+    });
   
- img.setAttribute('class', 'width : 256 ; src : https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png');
+  });
 
 
 
-  // ensure I append the right elements inside the correct ones
-
-  // once the card <li> is created and fully built up
-  // append it to the <ul>
-  ul.appendChild(li);
-  ul.appendChild(h2);
-}
-createAndAppendPokemonCard(data[0]);
-// for loop, to go through each pokemon in `data`
-// and for each pokemon, call createAndAppendPokemonCard
